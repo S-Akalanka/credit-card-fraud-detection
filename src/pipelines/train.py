bd9925f6574af321c86b24522a4002a776da9823
Akalanka <sasiri.akalankaa@gmail.com>
2026-06-09 18:38:17 +0530

    feat(logger): add logger utility for structured logging

--- src/pipelines/train.py
+++ src/pipelines/train.py
@@ -1,5 +1,15 @@
 import mlflow
+
+from pathlib import Path
 from src.utils.config import load_config
+from src.utils.logger import get_logger
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+LOG_PATH = PROJECT_ROOT / 'logs' / 'pipelines.log'
+
+logger = get_logger(__name__, log_file= LOG_PATH)
+
 
 def run_training():
 
@@ -8,6 +18,7 @@ def run_training():
     mlflow.set_tracking_uri(cfg.mlflow.tracking_uri)
     mlflow.set_experiment(cfg.mlflow.experiment_name)
 
+    logger.info("Ingesting data...")
 
 if __name__ == "__main__":
     run_training()
---
+++ src/utils/logger.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+from pathlib import Path
+
+
+def get_logger(name: str, log_file: str | None = None) -> logging.Logger:
+
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.DEBUG)
+
+    fmt = logging.Formatter(
+        fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    console.setFormatter(fmt)
+    logger.addHandler(console)
+
+    if log_file:
+        Path(log_file).parent.mkdir(parents=True, exist_ok=True)
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(fmt)
+        logger.addHandler(fh)
+
+    return logger
